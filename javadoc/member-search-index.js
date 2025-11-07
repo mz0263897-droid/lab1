f9724a0cc655e68b7ed17235631c5dacb377a129
mz0263897-droid <237619120+mz0263897-droid@users.noreply.github.com>
2025-11-07 12:37:26 +0200

    lab1 code

---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Lab1BylanynKI305","l":"Lab1BylanynKI305()","u":"%3Cinit%3E()","k":"3"},{"p":"<Unnamed>","c":"Lab1BylanynKI305","l":"main(String[])","u":"main(java.lang.String[])","k":"6"}];updateSearchResults();
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"Lab1BylanynKI305"}];updateSearchResults();
